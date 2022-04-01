6e9300458c4e6a5b10d327aabaafa7a72ce6afb6
Alysha Reinard <alysha.reinard@gmail.com>
2022-04-01 18:24:28 +0200

    changed column name and ran down a bug

--- dedup.py
+++ dedup.py
@@ -3,7 +3,13 @@ import pandas as pd
 import re
 
 def clean_URL(url):
-    return(re.search('https?://([A-Za-z_0-9.-]+).*', url))
+
+    try:
+        url = str(url)
+        clean = re.match('https?://([A-Za-z_0-9.-]+).*', url).group(0)
+        return(clean)
+    except:
+        return("")
 
 def process_files(badurl_file, project_file):
     badURL_df = pd.read_csv(badurl_file)
@@ -11,30 +17,32 @@ def process_files(badurl_file, project_file):
     badurls = []
     for i in range(len(badURL_df)):
     #    print("item", badURL_df["Main domain name"][i])
-        try:
-            badurls.append(clean_URL(badURL_df["Main domain name"][i]))
-        except:
-            pass
 
+        badurls.append(clean_URL(badURL_df["Website"][i]))
+
+    print("badURLS", badurls)
     good_records = []
     for i in range(len(project_df)):
     #    print(projectrecords['Main domain name'][i])
-        try:
-            p_url = clean_URL(project_df['Main domain name'][i])
-            if p_url not in badurls:
-                good_records.append(i)
-        except:
-            pass
+        if 'Website' not in project_df:
+            st.write("table must have column header 'Website'")
+#        print(type(project_df['Website'][i]))
+        p_url = clean_URL(project_df['Website'][i])
+        if p_url == "" or p_url not in badurls:
+            good_records.append(i)
 
-        
-        output_file = project_df.loc[good_records].reset_index(drop=True)
+
+    output_file = project_df.loc[good_records].reset_index(drop=True)
     return(output_file)
 
 
 st.title("Let's remove some bad URLs")
 
+
+#st.write("Upload your bad URL file here")
 badURLfile = st.file_uploader("Upload your badURLS")
 
+#st.write("Upload your project file here")
 projectfile = st.file_uploader("Upload your project records")
 
 time_to_process = st.button("Ready to process")
