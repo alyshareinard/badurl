27b9e81794e4aa0f25d79f5c7d4bb9db6fbf55a8
Alysha Reinard <alysha.reinard@gmail.com>
2022-04-01 09:39:33 +0200

    added button to process and put all processing behind that

--- dedup.py
+++ dedup.py
@@ -5,7 +5,9 @@ import re
 def clean_URL(url):
     return(re.search('https?://([A-Za-z_0-9.-]+).*', url))
 
-def process_files(badurls, projectrecords):
+def process_files(badurl_file, project_file):
+    badURL_df = pd.read_csv(badurl_file)
+    project_df=pd.read_csv(project_file)
     badurls = []
     for i in range(len(badURL_df)):
     #    print("item", badURL_df["Main domain name"][i])
@@ -15,47 +17,45 @@ def process_files(badurls, projectrecords):
             pass
 
     good_records = []
-    for i in range(len(projectrecords)):
+    for i in range(len(project_df)):
     #    print(projectrecords['Main domain name'][i])
         try:
-            p_url = clean_URL(projectrecords['Main domain name'][i])
+            p_url = clean_URL(project_df['Main domain name'][i])
             if p_url not in badurls:
                 good_records.append(i)
         except:
             pass
 
         
-        output_file = projectrecords.loc[good_records].reset_index(drop=True)
+        output_file = project_df.loc[good_records].reset_index(drop=True)
     return(output_file)
 
-badURL_df = pd.DataFrame()
-projectrecord= pd.DataFrame()
 
 st.title("Let's remove some bad URLs")
 
 badURLfile = st.file_uploader("Upload your badURLS")
-badURL_df = pd.read_csv(badURLfile)
 
-projectrecords = pd.read_csv(st.file_uploader("Upload your project records"))
+projectfile = st.file_uploader("Upload your project records")
 
-if (not badURL_df.empty) and (not projectrecords.empty):
-    process_files(badURL_df, projectrecords)
-
-
-#print(badURL_df)
+time_to_process = st.button("Ready to process")
+if time_to_process:
+    output_file = process_files(badURLfile, projectfile)
 
 
-@st.cache
-def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+    #print(badURL_df)
 
-csv = convert_df(output_file)
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name='large_df.csv',
-    mime='text/csv',
-)
+    @st.cache
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+
+    csv = convert_df(output_file)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='large_df.csv',
+        mime='text/csv',
+    )
 
